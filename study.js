f642fce9d8b2beee43a3b11d636b773ab31493be
Samuel Park <135080189+parksamuelsoung@users.noreply.github.com>
2025-06-14 21:44:09 -0400

    Create study.js

---
+++ study.js
@@ -0,0 +1,8 @@
+    const bibleVerses = {
+      "Genesis": { "4": { "26": "And to Seth also a son was born..." } },
+      "Joel": { "2": { "32": "And everyone who calls on the name..." } },
+      "John": { "3": { "16": "For God so loved the world...", "17": "For God did not send the Son..." } },
+      "Acts": { "2": { "21": "And it shall be that everyone..." } },
+      "Romans": { "10": { "12": "For there is no distinction...", "13": "For 'whoever calls...'" } },
+      "Galatians": { "1": { "15": "But when it pleased God, who set me apart from my mother’s womb and called me through His grace," } }
+    };
