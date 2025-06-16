eb861b7cc3002ffc8f199fa0b028b4187d051f3c
Samuel Park <135080189+parksamuelsoung@users.noreply.github.com>
2025-06-15 23:28:47 -0400

    Update study.js

--- study.js
+++ study.js
@@ -1,13 +1,23 @@
 const bibleVerses = {
   "Genesis": { "4": { "26": "And to Seth also a son was born..." } },
   "Joel": { "2": { "32": "And everyone who calls on the name..." } },
-  "John": { "3": { "16": "For God so loved the world...", "17": "For God did not send the Son..." } },
+  "John": { 
+    "3": { 
+      "16": "For God so loved the world...", 
+      "17": "For God did not send the Son..." 
+    } 
+    "4": {
+      "14": "But whoever drinks of the water that I will give him shall by no means thirst forever; but the water that I will give him will become in him a fountain of water springing up into eternal life."
+  },
   "Acts": { "2": { "21": "And it shall be that everyone..." } },
   "Romans": { "10": { "12": "For there is no distinction...", "13": "For 'whoever calls...'" } },
-  "Galatians": { "1": { "15": "But when it pleased God, who set me apart from my mother’s womb and called me through His grace,", 
-                        "16": "To reveal His Son in me that I might announce Him as the gospel among the Gentiles, immediately I did not confer with flesh and blood,"
-                      },
-                 "2": { "20": "I am crucified with Christ; and it is no longer I who live, but it is Christ who lives in me; and the life which I now live in the flesh I live in faith, the faith of the Son of God, who loved me and gave Himself up for me."
-                      }
-               }
+  "Galatians": { 
+    "1": { 
+      "15": "But when it pleased God, who set me apart from my mother’s womb and called me through His grace,", 
+      "16": "To reveal His Son in me that I might announce Him as the gospel among the Gentiles, immediately I did not confer with flesh and blood,"
+    },
+    "2": { 
+      "20": "I am crucified with Christ; and it is no longer I who live, but it is Christ who lives in me; and the life which I now live in the flesh I live in faith, the faith of the Son of God, who loved me and gave Himself up for me."
+    }
+  }
 };
